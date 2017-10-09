4c0a95fcec4a076a6681b68230e7935d39c8fa1d
chodges7 <cdhodges5@gmail.com>
2017-10-09 16:05:20 -0700

    Added film grain and boder option and grain functionality

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 int main(){
 
 char input;
+srand( time(NULL) );
 
 do{
 
@@ -27,42 +28,129 @@ std::cin >> title;
 image.open(title);
 std::vector <std::vector <Pixel> > bmp;
 
-if (image.isImage() == 1){
-    bmp = image.toPixelMatrix();
-    std::cout << title << " has been loaded.\n";
-    std::cout << "It is " << bmp[0].size();
-    std::cout << " wide and " << bmp.size() << " pixels high\n";
+std::cout << "Would you like a filter on your image?\n";
+std::cout << "(F)ilm grain\n" << "(B)order\n" << "(N)o\n";
+std::cin >> input;
 
-    for (int rodex = 0; rodex < bmp.size(); rodex++){
-        int tempRed;
-        int tempBlue;
-        int tempGreen;
-        int avg;
-        for (int coldex = 0; coldex < bmp[0].size(); coldex++){
-            rgb = bmp[rodex][coldex];
-            tempRed = rgb.red;
-            tempBlue = rgb.blue;
-            tempGreen = rgb.green;
-            avg = tempRed + tempBlue + tempGreen;
-            avg = avg / 3;
-            rgb.red = avg;
-            rgb.blue = avg;
-            rgb.green = avg;
-            bmp[rodex][coldex] = rgb;
+//Black and white version
+
+if (input == 'n' || input == 'N'){
+    if (image.isImage() == 1){
+        bmp = image.toPixelMatrix();
+        std::cout << title << " has been loaded.\n";
+        std::cout << "It is " << bmp[0].size();
+        std::cout << " wide and " << bmp.size() << " pixels high\n";
+
+        for (int rodex = 0; rodex < bmp.size(); rodex++){
+            int tempRed;
+            int tempBlue;
+            int tempGreen;
+            int avg;
+            for (int coldex = 0; coldex < bmp[0].size(); coldex++){
+                rgb = bmp[rodex][coldex];
+                tempRed = rgb.red;
+                tempBlue = rgb.blue;
+                tempGreen = rgb.green;
+                avg = tempRed + tempBlue + tempGreen;
+                avg = avg / 3;
+                rgb.red = avg;
+                rgb.blue = avg;
+                rgb.green = avg;
+                bmp[rodex][coldex] = rgb;
+            }
         }
-    }
     
-    image.fromPixelMatrix(bmp);
-    image.save("oldtimey.bmp");
-    std::cout << "Conversion done\n";
-
+        image.fromPixelMatrix(bmp);
+        image.save("oldtimey.bmp");
+        std::cout << "Conversion done\n";
+    }
 }
+
+//Film grain filter
+
+else if (input == 'f' || input == 'F'){
+    if (image.isImage() == 1){
+        bmp = image.toPixelMatrix();
+        std::cout << title << " has been loaded.\n";
+        std::cout << "It is " << bmp[0].size();
+        std::cout << " wide and " << bmp.size() << " pixels high\n";
+
+        int randomNum;
+
+        for (int rodex = 0; rodex < bmp.size(); rodex++){
+            int tempRed;
+            int tempBlue;
+            int tempGreen;
+            int avg;
+            for (int coldex = 0; coldex < bmp[0].size(); coldex++){
+                
+                randomNum = rand() % 1000;
+
+                if (randomNum == 0){
+                    rgb = bmp[rodex][coldex];
+                    rgb.red = 0;
+                    rgb.blue = 0;
+                    rgb.green = 0;
+                    bmp[rodex][coldex] = rgb;
+                }
+                else if (randomNum != 0){
+                    rgb = bmp[rodex][coldex];
+                    tempRed = rgb.red;
+                    tempBlue = rgb.blue;
+                    tempGreen = rgb.green;
+                    avg = tempRed + tempBlue + tempGreen;
+                    avg = avg / 3;
+                    rgb.red = avg;
+                    rgb.blue = avg;
+                    rgb.green = avg;
+                    bmp[rodex][coldex] = rgb;
+                }
+            }
+        }
+    
+        image.fromPixelMatrix(bmp);
+        image.save("oldtimegrainy.bmp");
+        std::cout << "Conversion done\n";
+    }
+}
+
+//Border filter
+
+else if (input == 'b' || input == 'B'){
+    if (image.isImage() == 1){
+        bmp = image.toPixelMatrix();
+        std::cout << title << " has been loaded.\n";
+        std::cout << "It is " << bmp[0].size();
+        std::cout << " wide and " << bmp.size() << " pixels high\n";
+
+        for (int rodex = 0; rodex < bmp.size(); rodex++){
+            int tempRed;
+            int tempBlue;
+            int tempGreen;
+            int avg;
+            for (int coldex = 0; coldex < bmp[0].size(); coldex++){
+                rgb = bmp[rodex][coldex];
+                tempRed = rgb.red;
+                tempBlue = rgb.blue;
+                tempGreen = rgb.green;
+                avg = tempRed + tempBlue + tempGreen;
+                avg = avg / 3;
+                rgb.red = avg;
+                rgb.blue = avg;
+                rgb.green = avg;
+                bmp[rodex][coldex] = rgb;
+            }
+        }
+    
+        image.fromPixelMatrix(bmp);
+        image.save("oldtimeborder.bmp");
+        std::cout << "Conversion done\n";
+    }
+}
+
 else if (image.isImage() == 0){
     std::cout << "That file isn't a .bmp format. Please enter a different file.\n";
 }
-
-
-
 
 /*
 Analyze every pixel in the bitmap using a 'for' loop that looks at the color of the pixels
